6299c8d5c0556909f33a124cc6765ec1ad2f2a81
hoang-gtvt <hoang92.gtvt@gmail.com>
2021-07-23 13:40:01 +0700

    xong demo

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+// import { NgModule } from '@angular/core';
+// import { RouterModule, Routes } from '@angular/router';
+// import {ListComponent} from "./user/list/list.component";
+// import {CreateComponent} from "./user/create/create.component";
+// import {EditComponent} from "./user/edit/edit.component";
+//
+// const routes: Routes = [
+//   {path:"home", component: ListComponent},
+//   {path:"create", component: CreateComponent},
+//   {path:"edit", component: EditComponent},
+//   {path:"delete", component: EditComponent}
+//
+// ];
+//
+// @NgModule({
+//   imports: [RouterModule.forRoot(routes)],
+//   exports: [RouterModule]
+// })
+// export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,40 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ListComponent } from './user/list/list.component';
+import { DetailComponent } from './user/detail/detail.component';
+import { CreateComponent } from './user/create/create.component';
+import { EditComponent } from './user/edit/edit.component';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {HttpClientModule} from "@angular/common/http";
+import {RouterModule} from "@angular/router";
+import { DeleteComponent } from './user/delete/delete.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListComponent,
+    DetailComponent,
+    CreateComponent,
+    EditComponent,
+    DeleteComponent,
+
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    RouterModule.forRoot([
+      {path:"home", component: ListComponent},
+      {path:"create", component: CreateComponent},
+      {path:"edit/:id", component: EditComponent},
+      {path:"delete/:id", component: DeleteComponent}
+
+    ])
+
   ],
   providers: [],
   bootstrap: [AppComponent]
